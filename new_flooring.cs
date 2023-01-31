2cab7f3025a5dd253a979300f4b4881f985d6c59
Laurits Bentsen <laurits2@hotmail.com>
2023-01-31 23:06:04 +0100

    COSTPERHOUR const changed to median labour value, all UI is now contained in methods

--- new_flooring.cs
+++ new_flooring.cs
@@ -4,59 +4,62 @@ namespace newFlooring
 {
     class Program
     {
-        const double COSTPERHOUR = 86.00;
+        const double COSTPERHOUR = 22.00;
         const int SQUAREFEETPERHOUR = 20;
 
         static void Main(string[] args)
         {
+            double hourlyCost = COSTPERHOUR;
             IntroMessage();
 
             int amountOfCorners = UserCorners();
 
             while (amountOfCorners != 3 && amountOfCorners != 4)
             {
-                Console.WriteLine("That is not a valid input");
-                Console.WriteLine("Please enter the number of corners in the room: ");
-                amountOfCorners = Convert.ToInt32(Console.ReadLine());
+                amountOfCorners = UserCorners();
             }
 
-            double costPerUnit = UserCostPerUnit();
-
             double area = 0;
+            double costPerUnit = UserCostPerUnit();
 
             if (amountOfCorners == 3)
             {
-                Console.WriteLine("Please enter the length of the base: ");
-                double baseLength = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Please enter the vertical length from the base: ");
-                double vertHeight = Convert.ToInt32(Console.ReadLine());
-
-                area = 0.5 * baseLength * vertHeight;
+                area = AreaOfTriangle();
             }
             if (amountOfCorners == 4)
             {
-                Console.WriteLine("Please enter the width of the floor: ");
-                double widthLength = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Please enter the length of the floor: ");
-                double heightLength = Convert.ToInt32(Console.ReadLine());
-
-                area = widthLength * heightLength;
+                area = AreaOfSquare();
             }
-            double cost = area / SQUAREFEETPERHOUR * COSTPERHOUR;
-            Console.WriteLine($"The total cost is: {cost}.");
+
+            OutputMessage(area);
         }
 
         static void IntroMessage()
         {
-            Console.WriteLine("Hello User, this program can calculate the cost of flooring when given a few inputs.");
-            Console.WriteLine("It can only be used with the following two shapes: triangle and rectangle.");
+            Console.WriteLine("Hello User, this program can calculate the cost of flooring work when given a few inputs.");
+            Console.WriteLine("It can only be used with the following shapes: triangle and rectangle (square/ parallelogram)");
+        }
+
+        static void OutputMessage(double area)
+        {
+            Console.WriteLine("Please enter the cost of labour per hour, if you dont know, an average will be used: ");
+            double hourlyCost = Convert.ToInt32(Console.ReadLine());
+
+            if (hourlyCost.Equals(null))
+            {
+                double cost = area / SQUAREFEETPERHOUR * COSTPERHOUR;
+                Console.WriteLine($"The total cost is: {cost}.");
+            }
+            else
+            {
+                double cost = area / SQUAREFEETPERHOUR * hourlyCost;
+                Console.WriteLine($"The total cost is: {cost}.");
+            }
         }
 
         static int UserCorners()
         {
-            Console.WriteLine("Please enter the number of corners in the room: ");
+            Console.WriteLine("Please enter a valid number of corners in the room: ");
             int corners = Convert.ToInt32(Console.ReadLine());
             return corners;
         }
@@ -66,6 +69,30 @@ namespace newFlooring
             Console.WriteLine("Please enter the cost pr. unit of flooring: ");
             double costPerUnit = Convert.ToDouble(Console.ReadLine());
             return costPerUnit;
+        }
+
+        static double AreaOfTriangle()
+        {
+            Console.WriteLine("Input length of base: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input length of height: ");
+            double h = Convert.ToInt32(Console.ReadLine());
+
+            double a = 0.5 * b * h;
+            return a;
+        }
+
+        static double AreaOfSquare()
+        {
+            Console.WriteLine("Please enter the width of the floor: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the length of the floor: ");
+            double h = Convert.ToInt32(Console.ReadLine());
+
+            double a = b * h;
+            return a;
         }
     }
 }
